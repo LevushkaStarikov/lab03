aac89a5e5221fa8908d3afc86165d5122e78979c
LevushkaStarikov <lev-starikov@mail.ru>
2022-05-15 01:42:08 +0300

    Создание библ formatter1

---
+++ formatter_lib/build/CMakeFiles/formatterlib.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/formatterlib.dir/formatter.cpp.o"
+  "CMakeFiles/formatterlib.dir/formatter.cpp.o.d"
+  "libformatterlib.a"
+  "libformatterlib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/formatterlib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
